ea7c800826f515f76b9771251deef582d3240a54
Khyber Sen <kkysen@gmail.com>
2018-01-17 15:04:39 -0500

    Commented out unnecessary SHA headers

--- src/util/hash.h
+++ src/util/hash.h
@@ -11,7 +11,7 @@
 
 typedef unsigned char byte;
 
-extern byte sha256_md[SHA256_DIGEST_LENGTH];
+/*extern byte sha256_md[SHA256_DIGEST_LENGTH];
 
 void sha256_r(const void *bytes, size_t size, void *digest);
 
@@ -21,7 +21,7 @@ void fprint_sha256(FILE *file, const void *bytes);
 
 int send_sha256(int fd, const void *bytes, size_t size);
 
-int verify_sha256(int fd, const void *bytes, size_t size, const char *file_type);
+int verify_sha256(int fd, const void *bytes, size_t size, const char *file_type);*/
 
 typedef union {
     double d;
