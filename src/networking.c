58b861a3383f5b45cd4e46e6545d5762ad5a1ae9
Khyber Sen <kkysen@gmail.com>
2018-01-23 23:38:30 -0500

    Added networking util functions in networking.h that bind, listen, and connect to IP addresses and ports.

--- src/networking.c
+++ src/networking.c
@@ -2,8 +2,80 @@
 // Created by kkyse on 1/23/2018.
 //
 
-#include <netdb.h>
 #include "networking.h"
+
+#include <netdb.h>
+#include <stdio.h>
+
+#include "util/utils.h"
+
+typedef int (*ConnectOrBind)(int socket_fd, const struct sockaddr *address, int address_length);
+
+static int open_socket(IpPort ip_port, const ConnectOrBind connect_or_bind) {
+    if (!(connect_or_bind == connect || connect_or_bind == bind)) {
+        perror("connect_or_bind must be connect() or bind()");
+        return -1;
+    }
+    
+    struct addrinfo hints = {
+            .ai_family = AF_UNSPEC, // IPv4 or IPv6
+            .ai_socktype = SOCK_DGRAM, // UDP
+            .ai_flags = AI_PASSIVE, // let syscall fill in
+    };
+    struct addrinfo *result;
+    
+    const int status = getaddrinfo(ip_port.ip_address, ip_port.port, &hints, &result);
+    if (status != 0) {
+        fprintf(stderr, "%s: %s\n", gai_strerror(status), "getaddrinfo(NULL, port, &hints, &result)");
+    }
+    
+    // really should walk the results linked list
+    const int socket_fd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (socket_fd == -1) {
+        freeaddrinfo(result);
+        check_msg(socket_fd, "socket(result->ai_family, result->ai_socktype, result->ai_protocol)");
+    }
+    
+    if (connect_or_bind(socket_fd, result->ai_addr, result->ai_addrlen) == -1) {
+        freeaddrinfo(result);
+        check_msg(-1, "connect_or_bind(socket_fd, result->ai_addr, result->ai_addrlen)");
+    }
+    
+    return socket_fd;
+}
+
+int bind_to_socket(const char *const port) {
+    const IpPort ip_port = {
+            .ip_address = NULL,
+            .port = port,
+    };
+    
+    const int socket_fd = open_socket(ip_port, bind);
+    check_msg(socket_fd, "open_socket(ip_port, bind)");
+    
+    // allows socket to be reused
+    const int yes = 1;
+    check_perror(setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)));
+    
+    return socket_fd;
+}
+
+int connect_to_socket(IpPort ip_port) {
+    const int socket_fd = open_socket(ip_port, connect);
+    check_msg(socket_fd, "open_socket(ip_port, connect)");
+    return socket_fd;
+}
+
+static const int LISTENING_BACKLOG = 10;
+
+int listen_to_socket(const char *const port) {
+    const int socket_fd = bind_to_socket(port);
+    check_msg(socket_fd, "bind_to_socket(port)");
+    
+    check_perror(listen(socket_fd, LISTENING_BACKLOG));
+    
+    return socket_fd;
+}
 
 int f() {
     struct addrinfo hints = {
@@ -11,9 +83,23 @@ int f() {
             .ai_socktype = SOCK_DGRAM, // UDP
             .ai_flags = AI_PASSIVE, // let syscall fill in
     };
-    struct addrinfo *servinfo;
+    struct addrinfo *results;
     
     const char *const port = "3490";
     
-    const int status = getaddrinfo(NULL, port, &hints, &servinfo);
+    const int status = getaddrinfo(NULL, port, &hints, &results);
+    if (status != 0) {
+        fprintf(stderr, "%s: %s\n", gai_strerror(status), "getaddrinfo(NULL, port, &hints, &results)");
+    }
+    
+    // really should walk the results linked list
+    const int socket_fd = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
+    check_msg(socket_fd, "socket(results->ai_family, results->ai_socktype, results->ai_protocol)");
+    
+    check_perror(bind(socket_fd, results->ai_addr, results->ai_addrlen));
+    
+    const int yes = 1;
+    check_perror(setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)));
+    
+    freeaddrinfo(results);
 }
--- src/networking.h
+++ src/networking.h
@@ -5,4 +5,33 @@
 #ifndef CASTROPARTY_NETWORKING_H
 #define CASTROPARTY_NETWORKING_H
 
-#endif //CASTROPARTY_NETWORKING_H
+typedef struct {
+    const char *const ip_address;
+    const char *const port;
+} IpPort;
+
+/**
+ * Opens and binds to a socket to a local \param port.
+ *
+ * @param [in] port the local port to bind to
+ * @return the socket file descriptor, or -1 if error
+ */
+int bind_to_socket(const char *port);
+
+/**
+ * Opens, binds, and listens to a socket to a local \param port.
+ *
+ * @param [in] port the local port to listen to
+ * @return the socket file descriptor, or -1 if error
+ */
+int listen_to_socket(const char *port);
+
+/**
+ * Opens and connects to a socket to an \param ip_port.
+ *
+ * @param [in] ip_port the IP address and port to connect to
+ * @return the socket file descriptor, or -1 if error
+ */
+int connect_to_socket(IpPort ip_port);
+
+#endif // CASTROPARTY_NETWORKING_H
